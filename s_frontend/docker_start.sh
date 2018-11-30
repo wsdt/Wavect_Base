02b2e293eedfc718d0c9e762e5a7fc6c1e100edc
kevin <kevin.riedl.privat@gmail.com>
2018-11-30 05:51:16 +0100

    change commadns

--- s_frontend/docker_start.sh
+++ s_frontend/docker_start.sh
@@ -1,7 +1,7 @@
 docker build -t bekind/s_frontend .
 
 # docker run -p {outsideport}:{insideport} --name={container_name} {image}
-docker run -p 3000:3000  --name=bekind_s_frontend s_frontend
+docker run -p 3000:3000  --name=bekind_s_frontend bekind/s_frontend
 
 # docker start {container_name} (container has to be created with command above)
 # docker start bekind_s_frontend
