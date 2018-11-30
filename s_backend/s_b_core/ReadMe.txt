8e53c5d561765a6b546b35fc9141535140860d65
kevin <kevin.riedl.privat@gmail.com>
2018-11-30 04:50:41 +0100

    added docker support for backend

--- s_backend/s_b_core/ReadMe.txt
+++
@@ -1,4 +0,0 @@
-https://medium.com/@wendysanarwanto/setup-node-js-typescript-project-5d54e8b5fdd
-
-Use also Nodemon. 
-
---
+++ s_backend/s_b_core/docker_start.sh
@@ -0,0 +1,5 @@
+# docker run -p {outsideport}:{insideport} --name={container_name} {image}
+docker run -p 3080:8080  --name=bekind_s_b_core s_b_core
+
+# docker start {container_name} (container has to be created with command above)
+# docker start bekind_s_b_core
