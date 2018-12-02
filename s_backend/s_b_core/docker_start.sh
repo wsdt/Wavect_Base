212e11de15abd8c76f9a08d6378f3eaa066ca7b9
kevin <kevin.riedl.privat@gmail.com>
2018-12-02 23:37:20 +0100

    h

--- s_backend/s_b_core/docker_start.sh
+++ s_backend/s_b_core/docker_start.sh
@@ -1,7 +1,7 @@
-docker build -t bekind/s_b_core .
+docker build -t s_b_core .
 
 # docker run -p {outsideport}:{insideport} --name={container_name} {image}
-docker run -p 3080:8080  --name=bekind_s_b_core s_b_core
+docker run -p 3080:8080  --name=s_b_core_c s_b_core
 
 # docker start {container_name} (container has to be created with command above)
 # docker start bekind_s_b_core
