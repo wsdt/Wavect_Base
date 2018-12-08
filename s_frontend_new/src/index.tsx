2f2bd5b0ba4fa2548a5d8b8b126f60a735c69164
kevin <kevin.riedl.privat@gmail.com>
2018-12-08 22:30:06 +0100

    try

--- s_frontend_new/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { App } from "./components/App";
-import "./styles/style.scss";
-const ROOT = document.querySelector(".container");
-
-ReactDOM.render(<App name="ExampleProperty" />, ROOT);
