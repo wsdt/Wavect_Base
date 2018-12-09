cd32161cad5ff14b720ec8b1ecb789ff874c1fce
kevin <kevin.riedl.privat@gmail.com>
2018-12-09 04:13:03 +0100

    travis docker

--- s_backend/s_b_core/docker_start.sh
+++ s_backend/s_b_core/docker_start.sh
@@ -1,6 +1,8 @@
 docker build -t s_b_core .
 
-docker stop -t 150 s_b_core
+docker stop -t 150 s_b_core_c
+echo Docker container will get stopped in 150 seconds.
+
 # docker run -p {outsideport}:{insideport} --name={container_name} {image}
 docker run -p 3080:8080  --name=s_b_core_c s_b_core
 
