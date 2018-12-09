9652b75c74e940d996017ddcba567cb4aeb0935f
kevin <kevin.riedl.privat@gmail.com>
2018-12-09 04:08:03 +0100

    travis docker

--- s_backend/s_b_core/docker_start.sh
+++ s_backend/s_b_core/docker_start.sh
@@ -1,7 +1,8 @@
 docker build -t s_b_core .
 
+docker stop -t 150 s_b_core
 # docker run -p {outsideport}:{insideport} --name={container_name} {image}
-docker run -p 3080:8080  --name=s_b_core_c s_b_core && docker stop s_b_core
+docker run -p 3080:8080  --name=s_b_core_c s_b_core
 
 # docker start {container_name} (container has to be created with command above)
 # docker start bekind_s_b_core
