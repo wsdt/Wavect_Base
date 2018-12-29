7a2179d1d0eda92421b58c5e6a03f8567a06c9ac
kevin <kevin.riedl.privat@gmail.com>
2018-12-29 13:33:06 +0100

    test

--- s_frontend/src/index.tsx
+++ s_frontend/src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { App } from "./components/App";
-import "./scss/style.scss";
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { App } from "./components/App"
+import "./scss/style.scss"
 
-const ROOT = document.querySelector("#root");
+const ROOT = document.querySelector("#root")
 
-ReactDOM.render(<App name="ExampleProperty" />, ROOT);
+ReactDOM.render(<App name="ExampleProperty" />, ROOT)
 // Maybe add serviceWorker for better performance
