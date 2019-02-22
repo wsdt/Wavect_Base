df6e9fb2d383520ad1e6838aeca8f45fcf517d9f
sevensevener <1810653109@stud.fh-kufstein.ac.at>
2019-02-22 16:30:04 +0100

    Added React-Native Login

--- s_frontend/s_mobile/src/mvc/views/Login.tsx
+++ s_frontend/s_mobile/src/mvc/views/Login.tsx
@@ -1,59 +1,60 @@
 import {Formik} from "formik"
 import * as React from "react"
-import {TextInput, View} from "react-native"
-import {Button, Icon, Input} from "react-native-elements"
+import {TextInput, View, StyleSheet, ViewStyle} from "react-native"
+import {Button, Icon, Input, Text, Divider} from "react-native-elements"
 
 import "../../scss/base.scss"
-import "../../scss/login.scss"
 
 export default class Login extends React.Component {
-    public state = {form:{isLoading:false}}
-    
+    state = {isPressed: false};
+
     public render() {
 
         return (
-            <Formik initialValues={{email: "", password: ""}}
-                    onSubmit={() => console.log("Login request sent.")}>
-                {({values, handleChange}) => (
-                    <React.Fragment>
-                        <Input placeholder="E-Mail"
-                               rightIcon={{type: "feather", name: "mail"}}
-                               value={values.email}
-                               shake={true}
-                               onChangeText={handleChange("email")}/>
-                        <Input
-                            value={values.password}
-                            rightIcon={{type: "feather", name: "lock"}}
-                            onChangeText={handleChange("password")}
-                            placeholder="Password"
-                            shake={true}
-                            secureTextEntry={true}/>
+            <View style={styles.container}>
+                <Text style={styles.title}> Log In</Text>
 
-                        <Button icon={<Icon
-                            name="explore"
-                            size={15}
-                            color="white"
-                        />}
-                                title="Log In"
-                                iconRight
-                                onPress={() => this.handleSubmit(values)}
-                                loading={this.state.form.isLoading}/>
-                    </React.Fragment>
-                )}
-            </Formik>
+                <Input  label='Username' placeholder='Username'/>
+                <Input  label='Password' placeholder='Password'/>
+
+                <Button buttonStyle={styles.button} title='Login' type='solid' onPress={this.stateHandler} loading={this.state.isPressed}/>
+
+                <Button buttonStyle={styles.notRegistered} title='Not Registered yet?' type='clear' />
+
+            </View>
         )
     }
 
-    private handleSubmit = (values) => {
-        console.log("LOGGING IN.")
-        // TODO: send  values to server
+    private stateHandler = () => {
+        this.setState({isPressed: true});
+            setTimeout( () => {
+                this.setState({isPressed: false})
 
-        this.setState({form:{isLoading: true}})
-        setTimeout(() => {
-            this.setState({form:{isLoading: false}})
-        }, 2000)
-
-    }
+            }, 2000)
+    };
 }
 
+const styles = StyleSheet.create({
 
+    title: {
+        fontSize: 48,
+        color: 'gray',
+    },
+
+    container: {
+        justifyContent: 'center',
+        alignItems:'center',
+        paddingTop: 50,
+        flex: 1
+    },
+
+    button: {
+        marginTop: 25,
+        width: 75,
+    },
+
+    notRegistered: {
+        marginTop: 25,
+        width: 200,
+    }
+});
