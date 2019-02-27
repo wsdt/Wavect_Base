c87a330ea826335f1fe38c1cd1298ffbe0ebefed
wsdt <kevin.riedl.privat@gmail.com>
2019-02-27 17:44:18 +0100

    husky

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,7 +3,15 @@ const tasks = t => t.join(' && ')
 module.exports = {
 	hooks: {
 		"pre-commit": tasks([
-			"echo 'hi'",
+			"cd s_backend/s_b_core/",
+			"yarn start prettier",
+			"echo '## BACKEND DONE ##'",
+			"cd ../../s_frontend/s_web/",
+			"yarn start prettier",
+			"echo '## WEB DONE ##'",
+			"cd ../s_mobile/",
+			"yarn start prettier",
+			"echo '## MOBILE DONE ##'",
 		])
 	},
 }
