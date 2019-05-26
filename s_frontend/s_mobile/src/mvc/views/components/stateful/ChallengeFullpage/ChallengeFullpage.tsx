d77627435daa85c277f8806b6787d2ac382f57a3
sevensevener <1810653109@stud.fh-kufstein.ac.at>
2019-05-26 19:24:02 +0200

    nth

--- s_frontend/s_mobile/src/mvc/views/components/stateful/ChallengeFullpage/ChallengeFullpage.tsx
+++ s_frontend/s_mobile/src/mvc/views/components/stateful/ChallengeFullpage/ChallengeFullpage.tsx
@@ -1,16 +1,32 @@
 import * as React from "react"
 import { ChallengeBackground } from "../ChallengeBackground/ChallengeBackground"
 import { IChallengeFullPageState } from "./ChallengeFullpage.state"
+import BottomChallengeBar from "../../functional/BottomChallengeBar/BottomChallengeBar";
+import {View} from "react-native";
+import {BaseScreen} from "../../../pages/BaseScreen/BaseScreen";
 
 /** Try to always use PureComponent, as it might be better regarding performance! */
 
-export class ChallengeFullpage extends React.PureComponent<any, IChallengeFullPageState> {
-    public state:IChallengeFullPageState = {
-        backgroundImg: {uri: "https://images.pexels.com/photos/1116050/pexels-photo-1116050.jpeg?auto=compress&cs=tinysrgb"}
-    }
+export class ChallengeFullpage extends React.PureComponent<
+    any,
+    IChallengeFullPageState
+> {
+    public state: IChallengeFullPageState = {
+        backgroundImg: {
+            uri:
+                "https://images.pexels.com/photos/1116050/pexels-photo-1116050.jpeg?auto=compress&cs=tinysrgb",
+        },
+    };
 
     public render() {
         // TODO: Instead of nesting maybe try to set it as bg property to parent view (grayscale) or make an own component
-        return <ChallengeBackground backgroundImg={this.state.backgroundImg} />
+        return (
+            <View>
+                <ChallengeBackground backgroundImg={this.state.backgroundImg} />
+                <BottomChallengeBar headline={"Laugh2gether"} subline={"Bring einen Freund zum Lachen und mache dabei ein Video"}/>
+            </View>
+        );
     }
 }
+
+{/*<ChallengeFullpage/>*/}
