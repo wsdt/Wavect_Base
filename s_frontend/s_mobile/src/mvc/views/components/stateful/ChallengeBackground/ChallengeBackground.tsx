3e5ff90a74b15ec52c685e0fd0bd14907d538424
wsdt <kevin.riedl.privat@gmail.com>
2019-05-26 18:53:42 +0200

    challenge bg

---
+++ s_frontend/s_mobile/src/mvc/views/components/stateful/ChallengeBackground/ChallengeBackground.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+import {ImageBackground, StyleProp, ViewStyle} from "react-native"
+import p from "../../../../controllers/parseScss"
+import {IChallengeBackgroundProps} from "./ChallengeBackground.props"
+import * as styles from "./ChallengeBackground.scss"
+
+
+export class ChallengeBackground extends React.PureComponent<IChallengeBackgroundProps, any> {
+    public state = {
+        isImageGrayscale: true,
+    }
+
+    private getImageStyles():Array<StyleProp<ViewStyle>> {
+        const imageStyles:any = [p(styles.bgImage)]
+        if (this.state.isImageGrayscale) {
+            //imageStyles.push({tintColor:"gray"})
+        }
+
+        return imageStyles
+    }
+
+    public render() {
+
+        return <ImageBackground imageStyle={this.getImageStyles()} style={p(styles.container)}
+                         source={this.props.backgroundImg}/>
+    }
+}
