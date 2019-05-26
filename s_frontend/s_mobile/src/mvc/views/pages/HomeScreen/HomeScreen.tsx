38bc993ae7c839ea22dac6c422d378896300ba81
wsdt <kevin.riedl.privat@gmail.com>
2019-05-26 19:00:38 +0200

    h

--- s_frontend/s_mobile/src/mvc/views/pages/HomeScreen/HomeScreen.tsx
+++ s_frontend/s_mobile/src/mvc/views/pages/HomeScreen/HomeScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
-import {ChallengeFullpage} from "../../components/stateful/ChallengeFullpage/ChallengeFullpage";
+import {ChallengeFullpage} from "../../components/stateful/ChallengeFullpage/ChallengeFullpage"
 import { BaseScreen } from "../BaseScreen/BaseScreen"
-import BottomChallengeBar from "../../components/functional/BottomChallengeBar/BottomChallengeBar";
 
 export class HomeScreen extends React.Component<any, any> {
     public render(): React.ReactNode {
