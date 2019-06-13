4aeddf3273650020618e88ee3eb65e743b678c5b
wsdt <kevin.riedl.privat@gmail.com>
2019-06-13 14:28:34 +0200

    added comment

--- s_frontend/s_mobile/src/mvc/views/components/stateful/GrayColorImg/GrayColorImg.tsx
+++ s_frontend/s_mobile/src/mvc/views/components/stateful/GrayColorImg/GrayColorImg.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
-import {View} from "react-native"
+import { View } from "react-native"
 import styles from "./GrayColorImg.css"
-
 
 export class GrayColorImg extends React.PureComponent<any, any> {
     public state = {
@@ -9,9 +8,12 @@ export class GrayColorImg extends React.PureComponent<any, any> {
     }
 
     public render() {
-        const Img:any = React.Children.only(this.props.children)
-        return <>
-            <Img />
-        </>
+        const Img: any = React.Children.only(this.props.children)
+        // todo How to: https://stackoverflow.com/questions/32606221/how-to-convert-an-image-to-grayscale-in-react-native
+        return (
+            <>
+                <Img />
+            </>
+        )
     }
 }
