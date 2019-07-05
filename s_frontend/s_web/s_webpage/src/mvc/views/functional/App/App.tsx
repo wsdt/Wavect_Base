b1e9aa0dbb0153af5ef3b572c4aa404b32b79f77
wsdt <kevin.riedl.privat@gmail.com>
2019-07-05 23:54:23 +0200

    comingsoon: updated social-media links

--- s_frontend/s_web/s_webpage/src/mvc/views/functional/App/App.tsx
+++ s_frontend/s_web/s_webpage/src/mvc/views/functional/App/App.tsx
@@ -1,13 +1,15 @@
 import * as React from "react"
 import { NavRouter } from "../../system/NavRouter/NavRouter"
-import {TopNav} from "../TopNav/TopNav"
+import { TopNav } from "../TopNav/TopNav"
 
 // <any, any> to set constraints we can set interfaces for props, states
 export class App extends React.Component<any, any> {
     public render() {
-        return <>
-            <TopNav />
-            <NavRouter />
+        return (
+            <>
+                <TopNav />
+                <NavRouter />
             </>
+        )
     }
 }
