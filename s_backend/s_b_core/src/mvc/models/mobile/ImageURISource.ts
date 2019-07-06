221d3b384b615974df28a3a0954c62125bd76319
wsdt <kevin.riedl.privat@gmail.com>
2019-07-06 18:38:18 +0200

    remove s_b_core

--- s_backend/s_b_core/src/mvc/models/mobile/ImageURISource.ts
+++
@@ -1,5 +0,0 @@
-
-/** Minimalistic copy of ImageURISource to ensure type safety (react class) */
-export interface ImageURISource {
-    uri:string
-}
--- s_backend/s_b_core/src/routes/api/mobile/apiMobileRoutes.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express"
-
-const router = express.Router()
-
-import * as mobileV1 from "./v1/v1ApiMobileRoutes"
-router.use("/v1", mobileV1)
-
-export = router
